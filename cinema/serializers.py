edd20c5152b0a561fbfefb96685db7a722a9a09c
Timur Khvat <timurkhvat@gmail.com>
2025-05-22 10:32:46 +0200

    'Solution'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -28,16 +28,8 @@ class MovieSerializer(serializers.ModelSerializer):
 
 
 class MovieListSerializer(MovieSerializer):
-    genres = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field="name"
-    )
-    actors = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field="full_name"
-    )
+    genres = serializers.StringRelatedField(many=True)
+    actors = serializers.StringRelatedField(many=True)
 
 
 class MovieRetrieveSerializer(MovieSerializer):
